2d4886716943f3e0232a6dd135311ffbaf674d4f
Matt Carter <m@ttcarter.com>
2023-09-28 13:06:54 +1000

    fix: Stop Cors middleware from constantly appending to the router

--- middleware/cors.js
+++ middleware/cors.js
@@ -11,8 +11,11 @@ export default function CowboyMiddlewareCORS(headers) {
 		res.set(injectHeaders);
 
 		// Handle hits to OPTIONS '/' endpoint
-		req.router.options('/', (req, res) => {
-			return res.sendStatus(200);
-		});
+		if (!req.router.loadedCors) {
+			req.router.options('/', (req, res) => {
+				return res.sendStatus(200);
+			});
+			req.router.loadedCors = true; // Mark we've already done this so we don't keep tweaking the router
+		}
 	}
 }
