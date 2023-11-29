e9fba4e4405cdc53242df3ab952cfd24ac337fb8
Matt Carter <m@ttcarter.com>
2023-11-29 12:07:35 +1000

    fix: More robust validation middleware

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,10 +1,15 @@
 import joyful from '@momsfriendlydevco/joyful';
 
+/**
+* Run a Joi / Joyful validation function against a specific subkey within `req
+*
+* @param {String} subkey The subkey to run against
+* @param {Function|Object} Callback to use with Joyful to validate with
+* @returns {Void} Either a successful middleware cycle (if validatio succeeds) or a call to `res.status(400)` if failed
+*/
 export default function CowboyMiddlewareValidate(subkey, validator) {
 	return (req, res) => {
-		let joyfulResult = joyful({
-			[subkey]: validator,
-		});
+		let joyfulResult = joyful(req[subkey], validator);
 
 		if (joyfulResult !== true) { // Failed body validation?
 			return res
