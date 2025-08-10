394a5e693aa8b89396dd0df958941495621757e2
Matt Carter <m@ttcarter.com>
2025-08-11 08:40:35 +1000

    fix: parseJwt middleware would fault if the isJwt did not return a promisable

--- middleware/parseJwt.js
+++ middleware/parseJwt.js
@@ -3,6 +3,8 @@
 *
 * @param {Object} [options] Additional options to mutate behaviour
 * @param {Function} [options.isJwt] Async function, called as `(req, res)` to determine if the input is a JWT payload, defaults to checking the content-type header
+*
+* @returns {CowboyMiddleware}
 */
 export default function CowboyMiddlewareParseJwt(options) {
 	let settings = {
@@ -13,7 +15,7 @@ export default function CowboyMiddlewareParseJwt(options) {
 	};
 
 	return async (req, res) => {
-		if (await !settings.isJwt(req, res)) return;
+		if (!(await settings.isJwt(req, res))) return;
 
 		const base64 = req.text.split('.')[1].replace(/-/g, '+').replace(/_/g, '/');
 		req.body = JSON.parse(atob(base64));
